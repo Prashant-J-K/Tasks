d016d39151db31dc3ab1164e2c19e9d5a29a4019
Prashant-J-K <jkprashant8@gmail.com>
2026-06-04 19:25:27 +0530

    Task1 added

---
+++ script.js
@@ -0,0 +1,95 @@
+// Task1
+
+let studentName = "Prashant";
+let studentAge = 22;
+let studentCourse = "MERN Stack";
+
+console.log(studentName);                     //Prashant
+console.log(studentAge);                      //22
+console.log(studentCourse);                   //MERN Stack
+
+
+
+// Task2
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google";
+salary = 30000;
+
+console.log(company);                         //Google
+console.log(salary);                          //30000
+console.log(country);                         //India
+
+
+
+
+// Task3
+let name = prompt("What is your name?");
+
+console.log("Welcome " + name);               //Welcome Prashant
+
+
+
+// Task4
+let result = confirm("Do you know JavaScript?");
+
+console.log(result);                           //true or false
+
+
+
+// Task5
+
+let studentname = prompt("Enter Student Name");
+let studentage = prompt("Enter Student Age");
+let studentCity = prompt("Enter Student City");
+
+console.log("Name : " + studentname);         //Name : Prashant
+console.log("Age : " + studentage);           //Age : 22
+console.log("City : " + studentCity);         //City : Bangalore
+
+
+// Task6
+
+let course = "JavaScript";
+
+alert("Welcome to " + course);                 //Welcome to JavaScript
+
+
+// Task7
+
+document.writeln("Hello Student");            //Hello Student
+document.writeln("<br>");                     
+document.writeln("Welcome to JavaScript");    //Welcome to JavaScript
+
+
+// Task8
+
+let food = "Chicken Biryani";
+let color = "Black";
+let movie = "Leo";
+
+console.log("Food : " + food);                //Food : Chicken Biryani
+console.log("Color : " + color);              //Color : Black
+console.log("Movie : " + movie);              //Movie : Leo
+
+
+// Task9
+let Name = "Prashant";
+let age = 22;
+let phone = "9999999999";
+let email = "prashant@gmail.com";
+
+console.log("Name : " + Name);                //Name : Prashant
+console.log("Age : " + age);                  //Age : 22
+console.log("Phone : " + phone);              //Phone : 9999999999
+console.log("Email : " + email);              //Email : prashant@gmail.com
+
+
+// Task10
+
+let NaMe = prompt("Enter Your Name");
+let Course = prompt("Enter Course Name");
+
+alert("Hello " + NaMe + ", Welcome to " + Course + " Course");   //Hello Prashant, Welcome to JavaScript Course
